901cd981702acad8acd383afc172486fa0d21b90
Salizwa27 <salizwamayekiso27@gmail.com>
2021-05-16 13:46:02 +0200

    added more tests for specification

--- BikeRide/tests/specification/SpecificationTests.java
+++ BikeRide/tests/specification/SpecificationTests.java
@@ -27,4 +27,69 @@ public class SpecificationTests {
         rBikeTwo.ride(roadBike);
         assertEquals(roadBike.currentSpeed(),99);
     }
+
+    @Test
+    public void shouldBeAbleToUseBikeRideThreeWithRoadBike() {
+        BicycleSpecification roadBikeSpec = new BicycleSpecification(11, 4);
+        Bicycle roadBike = new BicycleFromSpec(roadBikeSpec);
+        BikeRide rBikeThree = new BikeRideThree(roadBike);
+        rBikeThree.ride(roadBike);
+        assertEquals(roadBike.currentSpeed(),154);
+    }
+
+    @Test
+    public void shouldBeAbleToUseBikeRideOneWithMountainBike() {
+        BicycleSpecification mountainBikeSpec = new BicycleSpecification(5, 3);
+        Bicycle mountainBike = new BicycleFromSpec(mountainBikeSpec);
+        BikeRide rBikeOne = new BikeRideOne(mountainBike);
+        rBikeOne.ride(mountainBike);
+        assertEquals(mountainBike.currentSpeed(),30);
+    }
+
+    @Test
+    public void shouldBeAbleToUseBikeRideTwoWithMountainBike() {
+        BicycleSpecification mountainBikeSpec = new BicycleSpecification(5, 3);
+        Bicycle mountainBike = new BicycleFromSpec(mountainBikeSpec);
+        BikeRide rBikeTwo = new BikeRideTwo(mountainBike);
+        rBikeTwo.ride(mountainBike);
+        assertEquals(mountainBike.currentSpeed(),45);
+    }
+
+    @Test
+    public void shouldBeAbleToUseBikeRideThreeWithMountainBike() {
+        BicycleSpecification mountainBikeSpec = new BicycleSpecification(5, 3);
+        Bicycle mountainBike = new BicycleFromSpec(mountainBikeSpec);
+        BikeRide rBikeThree = new BikeRideThree(mountainBike);
+        rBikeThree.ride(mountainBike);
+        assertEquals(mountainBike.currentSpeed(),70);
+    }
+
+    @Test
+    public void shouldBeAbleToUseBikeRideOneWithTandem() {
+        BicycleSpecification mountainBikeSpec = new BicycleSpecification(12, 7);
+        Bicycle tandem = new BicycleFromSpec(mountainBikeSpec);
+        BikeRide rBikeOne = new BikeRideOne(tandem);
+        rBikeOne.ride(tandem);
+        assertEquals(tandem.currentSpeed(),72);
+    }
+
+    @Test
+    public void shouldBeAbleToUseBikeRideTwoWithTandem() {
+        BicycleSpecification tandemSpec = new BicycleSpecification(12, 7);
+        Bicycle tandem = new BicycleFromSpec(tandemSpec);
+        BikeRide rBikeTwo = new BikeRideTwo(tandem);
+        rBikeTwo.ride(tandem);
+        assertEquals(tandem.currentSpeed(),108);
+    }
+
+    @Test
+    public void shouldBeAbleToUseBikeRideThreeWithTandem() {
+        BicycleSpecification mountainBikeSpec = new BicycleSpecification(12, 7);
+        Bicycle tandem = new BicycleFromSpec(mountainBikeSpec);
+        BikeRide rBikeThree = new BikeRideThree(tandem);
+        rBikeThree.ride(tandem);
+        assertEquals(tandem.currentSpeed(),168);
+    }
+
 }
+
